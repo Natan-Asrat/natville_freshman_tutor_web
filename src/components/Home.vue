0ec9ef89e45f561504819219c707ded93cf3332d
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-08-31 23:00:14 +0300

    playstore link

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@
       <div class="col-md-6 d-flex flex-column justify-content-center">
         <h1 class="display-4">Natville Tutor</h1>
         <p class="lead">This is my gift to you all who mainly are joining campus as Freshman. I have covered Logic and Critical Thinking, Psychology, and Applied Mathematics One in my tutorials. Access the videos by downloading my app on play store below! Thank you!</p>
-        <a href="https://play.google.com/store/apps/details?id=com.emicasolutions.emishopapp" target="_blank">
+        <a href="https://play.google.com/store/apps/details?id=com.emicasolutions.natvillefreshmantutor_inamharic" target="_blank">
           <img src="/play.png" alt="Download on Play Store" class="img-fluid mt-3" style="max-width: 50%;"/>
         </a>
       </div>
